1825632b59b2598e060402419161cbb27ce84031
sambweber <42612830+sambweber@users.noreply.github.com>
2022-05-04 13:53:01 +0100

    Update prep_track.R

--- R/prep_track.R
+++ R/prep_track.R
@@ -1,11 +1,49 @@
 
-# The adjust.duplicateTimes function taken from package trip
 
-adjust_duplicate_times <- function (time) {
-  dups <- duplicated(time)
+# ----------------------------------------------------------------------------------------------------
+# adjust_duplicate_times
+# ----------------------------------------------------------------------------------------------------
+
+#' This is just the \code{adjust.duplicateTimes} function from package \code{trip}
+
+#' @param A datetime vector of class \code{POSIX*} for correction
+#' 
+#' @return Returns vector with 1 second added to duplicate timestamps 
+
+# -------------------------------------------------------------------------------------------------------
+
+adjust_duplicate_times <- function (dt) {
+  dups <- duplicated(dt)
   if (any(dups)) {
-    time[dups] <- time[dups] + 1
-    time <- Recall(time)
+    dt[dups] <- dt[dups] + 1
+    dt <- Recall(dt)
   }
-  time
+  dt
 }
+
+# ----------------------------------------------------------------------------------------------------
+# fix_track
+# ----------------------------------------------------------------------------------------------------
+
+#' Fixes common problems in tracking data including fractional seconds, duplicated rows and dulicated timestamps.
+#' A tidy/sf implementation of the \code{trip::forceCompliance} function
+
+#' @param A datetime vector of class \code{POSIX*} for correction
+#' 
+#' @return Returns vector with 1 second added to duplicate timestamps 
+
+# -------------------------------------------------------------------------------------------------------
+
+fix_track = function (x, dt = 'datetime'){
+  
+  dt = as.name(dt)
+  if(is(x,'Spatial')) x = st_as_sf(x)   
+  
+  x %>%
+    mutate(!!dt := round(!!dt,'secs')) %>%    # deal with fractional seconds from some tags (e.g. FastGPS)
+    arrange(!!dt,.by_group=T) %>%             # order by datetime
+    dplyr::distinct(.keep_all=TRUE) %>%       # remove duplicate rows
+    mutate(!!dt := adjust.duplicateTimes(!!dt))
+  
+}
+

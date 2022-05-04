57a205ba131883b1a831a8f91a1a898663c3fa35
sambweber <42612830+sambweber@users.noreply.github.com>
2022-05-04 13:57:28 +0100

    Update prep_track.R

--- R/prep_track.R
+++ R/prep_track.R
@@ -6,7 +6,7 @@
 
 #' This is just the \code{adjust.duplicateTimes} function from package \code{trip}
 
-#' @param A datetime vector of class \code{POSIX*} for correction
+#' @param dt A datetime vector of class \code{POSIX*} for correction
 #' 
 #' @return Returns vector with 1 second added to duplicate timestamps 
 
@@ -28,9 +28,10 @@ adjust_duplicate_times <- function (dt) {
 #' Fixes common problems in tracking data including fractional seconds, duplicated rows and dulicated timestamps.
 #' A tidy/sf implementation of the \code{trip::forceCompliance} function
 
-#' @param A datetime vector of class \code{POSIX*} for correction
-#' 
-#' @return Returns vector with 1 second added to duplicate timestamps 
+#' @param x An object of class \code{Spatial} or \code{sf} containing track points for a single individual
+#' @param dt The name of the column containing the timestamp (must be of class \code{POSIX*}) 
+
+#' @return Returns the original spatial object with common problems fixed
 
 # -------------------------------------------------------------------------------------------------------
 
@@ -43,7 +44,7 @@ fix_track = function (x, dt = 'datetime'){
     mutate(!!dt := round(!!dt,'secs')) %>%    # deal with fractional seconds from some tags (e.g. FastGPS)
     arrange(!!dt,.by_group=T) %>%             # order by datetime
     dplyr::distinct(.keep_all=TRUE) %>%       # remove duplicate rows
-    mutate(!!dt := adjust.duplicateTimes(!!dt))
+    mutate(!!dt := adjust_duplicate_times(!!dt))
   
 }
 

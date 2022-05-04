e578d3eb30b4bd219c70895a7a54b436be94cee1
sambweber <42612830+sambweber@users.noreply.github.com>
2022-05-04 16:49:58 +0100

    Update crawl_tools.R

--- R/crawl_tools.R
+++ R/crawl_tools.R
@@ -1,4 +1,16 @@
 
+# ----------------------------------------------------------------------------------------------------
+# add_anisotopic_errors
+# ----------------------------------------------------------------------------------------------------
+
+#' A convenience wrapper around \code{crawl::argosDiag2Cov} that adds anisotropic errors back to a dataframe
+#' of tracking data in a form that \code{crawl::crwMLE} can use.
+
+#' @return The original dataframe with new columns ln.sd.x and ln.sd.y which are log(err/sqrt(2)), where err is
+#' Argos errors in metres, along with the covariances between them.
+
+# -------------------------------------------------------------------------------------------------------
+
 add_anisotopic_errors <- function(obj, Major = "Error.Semi.Major.Axis", Minor = "Error.Semi.Minor.Axis",
 Orientation = "Error.Ellipse.Orientation"){
 

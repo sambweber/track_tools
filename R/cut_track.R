cb467eb93ce40f917174e53b53c345da29642efb
sambweber <42612830+sambweber@users.noreply.github.com>
2022-05-13 16:22:25 +0100

    Update cut_track.R

--- R/cut_track.R
+++ R/cut_track.R
@@ -3,6 +3,10 @@ cut_track = function(data,datetime,tmax = NULL,cut.dates = NULL){
       mutate(data,t = traipse::track_time(!!sym(datetime))) %>%
       mutate(t = replace_na(t,0)) %>%
       mutate(segment = 1+cumsum(t > tmax*3600))
+     
+    # date supplied method
+     cut.dates = as.POSIXct(cut.dates)
+     rowwise(data) %>% mutate(segment = 1+sum(date > cut.dates)) %>% ungroup()
     
   }
   

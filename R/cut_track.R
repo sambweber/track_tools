20a4c9bf1af6b2931df583a9e318f4e982a7cbcb
sambweber <42612830+sambweber@users.noreply.github.com>
2022-05-15 22:26:55 +0100

    Update cut_track.R

--- R/cut_track.R
+++ R/cut_track.R
@@ -1,25 +1,37 @@
-cut_track = function(data,datetime,tmax = NULL,cut.dates = NULL){
+#' ------------------------------------------------------------------------------------------------------------------
+#' cut_track
+#' ------------------------------------------------------------------------------------------------------------------
+
+
+
+cut_track = function(data,datetime,tmax = NULL,cut.dates = NULL,overlap=F){
+  
+  if (!is(data[[datetime]],"POSIXt")) stop("'datetime' should be of class POSIXt")
+  
+  if(is.null(tmax) & is.null(cut.dates)) stop('Either tmax or cut.dates must be specified')
+  if(!is.null(tmax) & !is.null(cut.dates)) stop("'tmax' and 'cut.dates' both provided! Splitting by tmax.")
+  
+  if(!is.null(tmax)){   
     
-    datetime <- try(as.POSIXct(datetime), silent = TRUE)
-    if (is(datetime,"try-error")) stop("Cannot convert 'datetime' to POSIXct")
+    mutate(data,t = traipse::track_time(!!sym(datetime))) %>%
+    mutate(t = replace_na(t,0)) %>%
+    mutate(segment = 1+cumsum(t > tmax*3600)) %>%
+    dplyr::select(-t) %>% return()
     
-    if(is.null(tmax) & is.null(cut.dates)) stop('Either tmax or cut.dates must be specified')
-    if(!is.null(tmax) & !is.null(cut.dates)) stop("'tmax' and 'cut.dates' both provided! Splitting by tmax.")
+  } else {
     
-    if(!is.null(tmax)){   
-        
-        mutate(data,t = traipse::track_time(!!sym(datetime))) %>%
-        mutate(t = replace_na(t,0)) %>%
-        mutate(segment = 1+cumsum(t > tmax*3600)) %>%
-        return()
-        
-    } else {
-       
-        cut.dates = try(as.POSIXct(cut.dates),silent=TRUE)
-        if(is(cut.dates('try-error')) stop ("Cannot convert 'cut.dates' to POSIXct")
-        rowwise(data) %>% mutate(segment = 1+sum(datetime > cut.dates)) %>% 
-        ungroup() %>% return()
+    cut.dates = try(as.POSIXct(cut.dates,tz='GMT'),silent=TRUE)
+    if(is(cut.dates,'try-error')) stop ("Cannot convert 'cut.dates' to POSIXct")
+    data = rowwise(data) %>% mutate(segment = 1+sum(!!sym(datetime) > cut.dates)) 
     
+    if(overlap){
+      data = group_by(data,segment) %>% slice(1,n()) %>% 
+      mutate(segment = segment+c(-1,1)) %>% 
+      subset(!segment %in% c(0,length(cut.dates)+2)) %>%
+      bind_rows(.,data) %>% arrange(!!sym(datetime))
+    }
+      
+    return(ungroup(data))
   }
-        
-}  
+}
+  

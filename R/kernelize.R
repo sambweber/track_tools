d35b956f4b5c8990f186f05928a9a7fa238dcce5
sambweber <42612830+sambweber@users.noreply.github.com>
2022-06-19 03:01:25 +0100

    Update kernelize.R

--- R/kernelize.R
+++ R/kernelize.R
@@ -37,3 +37,44 @@ if(!missing(id)){
   
 }
 
+
+# -------------------------------------------------------------------------------------------------------
+# volume_contour
+# -------------------------------------------------------------------------------------------------------
+
+volume_contour <- function(input, res.out = 10, levels = c(95,75,25,50),output = c('raster','polygons')){
+  
+  require(raster); require(rgdal); require(sf); require(rgeos)
+  
+  output = match.arg(output)
+  
+  resamp = raster::disaggregate(x=input,fact = res.out,method="bilinear")
+  Z = raster::values(resamp)
+  resamp[] = Z/sum(Z,na.rm=T) # normalise 
+  
+  vclevels = sort(levels,decreasing = T) #sort the volume contour levels
+  rastvals = sort(raster::values(resamp)) #rank the raster probability values
+  breaks   = sapply(1-vclevels/100,FUN=function(x){rastvals[max(which(cumsum(rastvals)<=x))]})
+  breaks   = c(breaks,1)
+  
+  if(output == 'polygons'){
+    #create contour lines
+    vcs = rasterToContour(resamp,levels = breaks[1:length(levels)],maxpixels = ncell(resamp))
+    vcs$level = levels
+    
+    #convert to polygons 
+    vcpolys = try(st_as_sf(vcs) %>% st_cast('POLYGON')) 
+    if(class(vcpolys) == "try-error") vcpolys = st_as_sf(vcs) %>% st_polygonize() 
+    
+    vcpolys = st_transform(vcpolys,54034) %>% mutate(area_km2 = round(st_area(.)/1E6,1))
+    
+    return(vcpolys)
+    
+  } else {
+    
+    resamp = reclassify(resamp,matrix(c(min(rastvals),breaks[-length(breaks)],breaks,NA,vclevels),ncol=3),include.lowest=T)
+    return(resamp)
+    
+  }
+  
+}

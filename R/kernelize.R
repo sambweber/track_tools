fefd4d4f750e80d4678547cd6ae1d7a76257b691
sambweber <42612830+sambweber@users.noreply.github.com>
2022-06-19 03:08:04 +0100

    Update kernelize.R

--- R/kernelize.R
+++ R/kernelize.R
@@ -2,41 +2,45 @@
 # kernelize
 # ---------------------------------------------------------------------------------------
 
-kernelize = function(data,id,resolution,h=NULL,crs=NULL){
-
-if(is(data,'sf')) {
-
-  crs = st_crs(data)
-  data = dplyr::select(-any_of(c('X','Y'))) %>% 
-         cbind(st_coordinates(.)) %>%
-         st_set_geometry(NULL)
-  
-}
-
-if(!all(c('X','Y') %in% names(data))) stop ("data should contain columns 'X' and 'Y' containing coordinates")
-
-# compose grid
-lims = c(range(data$X),range(data$Y))
-r = extent(lims) %>% raster(resolution = resolution) %>% extend(1)
-lims = as.numeric(matrix(extent(r)))
-n = dim(r)[2:1]
-
-fit.k = function(d) {
-   k = MASS::kde2d(x = d$X, y = d$Y, n=n,lims = lims) %>% raster()
-   (k/cellStats(k,'sum'))
- }
-
-if(!missing(id)){
-
-    nest(data,crds = -!!id) %>%
-    mutate(kernel = map(crds,fit.k)) %>%
-    dplyr::select(-crds)
+kernelize <- function(data,id,resolution,h=NULL,crs=NULL,landmask=NULL){
     
-} else { fit.k(data) }
- 
-  
+    if(is(data,'sf')) {
+        
+        crs = st_crs(data)
+        data = dplyr::select(-any_of(c('X','Y'))) %>% 
+            cbind(st_coordinates(.)) %>%
+            st_set_geometry(NULL)
+        
+    }
+    
+    if(!is.null(landmask)){
+        land = try(as(land,'Spatial'))
+        if(is(land,'try-error')) stop('land should be coercible to class Spatial')
+    }
+    
+    if(!all(c('X','Y') %in% names(data))) stop ("data should contain columns 'X' and 'Y' containing coordinates")
+    
+    # compose grid
+    lims = c(range(data$X),range(data$Y))
+    r = extent(lims) %>% raster(resolution = resolution) %>% extend(1)
+    lims = as.numeric(matrix(extent(r)))
+    n = dim(r)[2:1]
+    
+    fit.k = function(d) {
+        k = MASS::kde2d(x = d$X, y = d$Y, n=n,lims = lims) %>% raster()
+        if(!is.null(landmask)) k = mask(k,land,inverse=T)
+        k = (k/cellStats(k,'sum'))
+    }
+    
+    if(!missing(id)){
+        
+        nest(data,crds = -!!id) %>%
+            mutate(kernel = map(crds,fit.k)) %>%
+            dplyr::select(-crds)
+        
+    } else { fit.k(data) }
+    
 }
-
 
 # -------------------------------------------------------------------------------------------------------
 # volume_contour
